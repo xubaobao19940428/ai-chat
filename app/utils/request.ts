848a514c90ab9a00504e291c84a26dfb591c514d
qiancheng <915775317@qq.com>
2026-01-22 22:58:34 +0800

    去掉调试代码

--- app/utils/request.ts
+++ app/utils/request.ts
@@ -26,7 +26,6 @@ const service: AxiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
     'x-app-id': import.meta.client ? useRuntimeConfig().public.appId : '1',
-    // 'x-app-key': import.meta.client ? useRuntimeConfig().public.appKey : '49f68a5c8493ec2c0bf489821c21fc3b',
     'x-app-domain': 'ai-test.iappdaily.com',
   },
 })
@@ -108,13 +107,13 @@ service.interceptors.request.use(
     const sign = md5(encodedFullPath + queryString + secretKey);
 
     // Debug Logs
-    console.log('=== Signature Debug ===');
-    console.log('Original Config URL:', config.url); // Check if it includes /api
-    console.log('Full Path:', fullPath);
-    console.log('Sorted Params:', sortedParams);
-    console.log('String to Sign:', encodedFullPath + queryString + secretKey);
-    console.log('Generated Sign:', sign);
-    console.log('=======================');
+    // console.log('=== Signature Debug ===');
+    // console.log('Original Config URL:', config.url); // Check if it includes /api
+    // console.log('Full Path:', fullPath);
+    // console.log('Sorted Params:', sortedParams);
+    // console.log('String to Sign:', encodedFullPath + queryString + secretKey);
+    // console.log('Generated Sign:', sign);
+    // console.log('=======================');
 
     // Update config
     config.url = baseUrl;
@@ -137,9 +136,9 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   (response: AxiosResponse) => {
     const res = response.data as any
-    console.log('=== Response Debug ===');
-    console.log('Response Data:', res);
-    console.log('=======================');
+    // console.log('=== Response Debug ===');
+    // console.log('Response Data:', res);
+    // console.log('=======================');
     // 根据实际后端接口调整
     if (res && typeof res === 'object' && 'code' in res && res.code !==0) {
       return Promise.reject(new Error(res.message || 'Error'))
@@ -147,7 +146,7 @@ service.interceptors.response.use(
     return response.data
   },
   (error) => {
-    console.error('请求错误:', error)
+    console.error('request error:', error)
     return Promise.reject(error)
   }
 )
